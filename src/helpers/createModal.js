1133ec982da51f8629843566b84ad6f9ad6b9918
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2025-10-30 14:32:51 +0200

    cross
    Adding a window closing cross. Editing

--- src/helpers/createModal.js
+++ src/helpers/createModal.js
@@ -2,6 +2,7 @@ import * as basicLightbox from 'basiclightbox'
 import "basiclightbox/dist/basicLightbox.min.css";
 
 import { closeMadal } from './closeModal';
+import closeIcon from '../img/close.png';
 
 
 function createModal({img, name, price, description}){
@@ -21,6 +22,9 @@ function createModal({img, name, price, description}){
   
      const instance = basicLightbox.create(`
 	    <div class="modal">
+            <div class="cross">
+                 <img src=${closeIcon} alt="close">
+            </div>
             <img src="${img}" alt="${name}" width="300">
             <h2>${name}</h2>
             <h3>${price}point</h3>
@@ -36,22 +40,37 @@ function createModal({img, name, price, description}){
       console.log(this);
       this.handler = closeMadal.bind(instance);
     document.addEventListener('keydown', this.handler);
+
+ 
+ 
   },
-  onClose(){
-     console.log(this);
-     document.removeEventListener('keydown', this.handler);
+    onClose(){
+      console.log(this);
+      document.removeEventListener('keydown', this.handler);
+      cross.removeEventListener('click', toClose);
   },
   
 });
+
+
+// cross.addEventListener('click', toClose);
+// function toClose(){
+//   console.log('whoa!');
+// }
 instance.show();
 // function closeMadal(evt){
 //     if(evt.code === 'Escape'){
 //       instance.close();
 //     }
-// }
+  const cross = document.querySelector('.cross');
+    console.log(cross);
+    cross.addEventListener('click', toClose);
+  function toClose(){
+    console.log('whoa!');
+    instance.close();
+ }
 
 }
-
 
 
 export { createModal };
